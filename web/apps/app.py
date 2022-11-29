ed28b260a6acc22a6bf4b352a09bf518aafebc70
strongironheart <tayamash@redhat.com>
2022-11-29 11:43:32 +0900

    commit

--- web/apps/app.py
+++ web/apps/app.py
@@ -17,12 +17,85 @@ app = Flask(
     static_folder = "views/static",
     template_folder = "views/templates"
 )
+db = SQLAlchemy()
+mail = Mail()
+csrf = CSRFProtect()
+
+class Config:
+
+    # SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{dbname}?charset=utf8'.format(**{
+        'user': os.getenv('MYSQL_USER', ''),
+        'password': os.getenv('MYSQL_PASSWORD', ''),
+        'host': 'mysql_koz',
+        'dbname': 'hoge'
+    })
+
+
+    # Assets Management
+    ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
+
+    # セッションを使う際の秘密キー
+    SECRET_KEY = os.getenv('SECRET_KEY', '') 
+
+    # CSRFトークンの設定
+    WTF_CSRF_SECRET_KEY = os.getenv('CSRF_SECRET', '')
+    
+    # session
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+
+    WTF_CSRF_ENABLED = True
+
+    # jsonをそのままダンプする(ASCIIにしない)
+    JSON_AS_ASCII = False
+
+    # Gmail Setting
+    #MAIL_SERVER = 'smtp.gmail.com'
+    #MAIL_PORT = 587
+    #MAIL_USE_TLS =  True
+    #MAIL_USERNAME = os.getenv('MAIL_USERNAME', '')
+    #MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
+    #MAIL_DEFAULT_SENDER = 'FLASK-SAMPLE'
+
 
 def create_app():
+
+
+    # configの設定を反映する
+    app.config.from_object(Config())
+    # ログ
+    #if os.getenv('ENV') == 'PROD':
+    #    app.debug = False
+    #    app.logger.setLevel(logging.INFO)
+    #else:
+    #    app.debug = True
+    #    app.logger.setLevel(logging.DEBUG)
+
+    #app.logger.warning("hoge")
+
+    # CSRF対策
+    csrf.init_app(app)
+
+    # mail
+    #mail.init_app(app)
+
+    # databaseのインスタンスを作成する
+    db.init_app(app)
+    # Migration設定
+    Migrate(app, db)
 
     # blue printでルートを設定する
     from apps.pymodule.landing import index as landing_view
     app.register_blueprint(landing_view.landing)
+
+    from apps.pymodule.setting import index as setting_view
+    app.register_blueprint(setting_view.setting)
+
+    #from apps.pymodule.inqury import index as inqury_view
+    #app.register_blueprint(inqury_view.inqury)
+
+    #from apps.pymodule.mislenious import index as mislenious_view
+    #app.register_blueprint(mislenious_view.mislenious)
 
     return app
 
