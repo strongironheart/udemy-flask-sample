818132584e0178ba85814963255d0457d5ffa925
strongironheart <tayamash@redhat.com>
2022-11-30 18:06:33 +0900

    commit

--- web/apps/pymodule/setting/index.py
+++ web/apps/pymodule/setting/index.py
@@ -59,7 +59,7 @@ def money():
 
                 money:MONEY = MONEY(
                         # 認証認可が入ると、この処理はリクエストヘッダーから取得することができる
-                        system_id = check_jwt()
+                        system_id = 1111111
                 )
 
                 money_record = money.get_data()
@@ -69,7 +69,7 @@ def money():
                         # データがある場合とない場合で分けることができる
                         money_record = MONEY(
                                 # 認証認可が入ると、この処理はリクエストヘッダーから取得することができる
-                                system_id = check_jwt(),
+                                system_id = 1111111,
                                 soushisan =  form.soushisan.data
                         )
                 else:
